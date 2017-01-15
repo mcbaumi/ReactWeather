c8e63b8125f0e6074ee46bfcd277951b8c6b5ffc
Damien Baumgartner <baumi.devl@gmail.com>
2017-01-15 12:30:12 +0100

    Add custom styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>This is a weather app built with React.</p>
       <p>You can search the current temperature for cities.</p>
     </div>
